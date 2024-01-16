3d7e95ad8dec1ef5ba5ecf5570c2cfeb103d665f
Adam Kubik <kubik.adam445@gmail.com>
2024-01-17 00:05:42 +0100

    trying apis instrad of sql api

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,12 @@ export const metadata: Metadata = {
 export default async function Page() {
 
     const {rows}: {rows: CategoryType[]} = await sql`SELECT * FROM categories`
+    let categories: CategoryType[] = await fetch('https://marketplace-nextjs-roan.vercel.app/api/categories', {next: {tags: ["categories"]}}).then((res) => res.json()).then((data) => data['data'])
+    console.log(categories)
     return (
     <main>
         <ul className="flex flex-row flex-wrap gap-6 m-auto mx-2 xl:mx-80 mt-12">
-            {rows.map((category) => (
+            {categories.map((category) => (
                 <Category key={category.name} category={category} />
                 ))}
         </ul>
