c13d88a5e2f70659f5048ed7f6eea0aa0997cfb8
Adam Kubik <kubik.adam445@gmail.com>
2024-01-16 23:50:54 +0100

    new FIX

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
 import { CategoryType } from '../../schemas';
 import { NextResponse } from "next/server";
 
-export const revalidate = true
+export const revalidate = 0
 
 export async function GET(
     request: Request){
