e8f816168dfabbf3266857040d96e7661a555101
Adam Kubik <kubik.adam445@gmail.com>
2024-01-17 14:38:08 +0100

    reversing

--- app/helperfunctions/revalidateProducts.ts
+++ app/helperfunctions/revalidateProducts.ts
@@ -3,6 +3,5 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 export default async function revalidateProducts() {
     revalidatePath("/admin"),
     revalidatePath("/[category]", 'page'),
-    revalidatePath("/api/products/category/[category]"),
     revalidateTag("products")
 }
