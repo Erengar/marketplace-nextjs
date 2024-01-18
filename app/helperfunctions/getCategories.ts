7ccd39baed48350cd84238dbd7b754b30c2f2395
Adam Kubik <kubik.adam445@gmail.com>
2024-01-18 13:15:11 +0100

    attempt bug fix

--- app/helperfunctions/getCategories.ts
+++ app/helperfunctions/getCategories.ts
@@ -2,13 +2,6 @@ import { CategoryType } from "../schemas"
 
 
 export default async function getCategories(): Promise<CategoryType[]> {
-    let domain : string
-    /*
-    if (process.env.PRODUCTION) {
-        domain = "https://marketplace-nextjs-roan.vercel.app"
-    } else {
-        domain = "http://localhost:3000"
-    }*/
     const result = await fetch(`${process.env.DEPLOYMENT_URL}api/categories/`, {next: {tags: ["categories"]}})
     .then((res) => res.json())
     .then((data) => data['data'] as CategoryType[])
--- app/helperfunctions/getProducts.ts
+++ app/helperfunctions/getProducts.ts
@@ -2,12 +2,6 @@ import { ProductType } from "../schemas"
 
 
 export default async function getProducts(category: string): Promise<ProductType[]> {
-    let domain : string
-    if (process.env.vercel) {
-        domain = "https://marketplace-nextjs-roan.vercel.app"
-    } else {
-        domain = "http://localhost:3000"
-    }
     const result = await fetch(`${process.env.DEPLOYMENT_URL}api/products/category/${category}/`, {next: {tags: ["products"]}})
     .then(res => res.json())
     .then(data => data['data'] as ProductType[])
