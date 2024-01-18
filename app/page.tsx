d56d119ee5fa5e363b2f5537c30bfe2ac79d54e6
Adam Kubik <kubik.adam445@gmail.com>
2024-01-18 13:42:05 +0100

    reverse api call for sql fetch in server component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Category from './components/client/Category'
 import { Metadata } from 'next'
 import { CategoryType } from './schemas'
 import  getCategories  from './helperfunctions/getCategories'
+import { sql } from '@vercel/postgres'
 
 
 export const metadata: Metadata = {
@@ -10,12 +11,13 @@ export const metadata: Metadata = {
 
 
 export default async function Page() {
-    const categories: CategoryType[] = await getCategories()
+    //const categories: CategoryType[] = await getCategories()
+    const {rows}  = await sql<CategoryType>`SELECT * FROM categories`
 
     return (
     <main>
         <ul className="flex flex-row flex-wrap gap-6 m-auto mx-2 xl:mx-80 mt-12">
-            {categories.map((category) => (
+            {rows.map((category) => (
                 <Category key={category.name} category={category} />
                 ))}
         </ul>
