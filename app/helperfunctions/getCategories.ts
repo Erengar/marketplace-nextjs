d86538f87841b08a8bf4a1918a82cd429d57dfde
Adam Kubik <kubik.adam445@gmail.com>
2024-01-18 13:23:37 +0100

    test whether raw url works

--- app/helperfunctions/getCategories.ts
+++ app/helperfunctions/getCategories.ts
@@ -2,7 +2,7 @@ import { CategoryType } from "../schemas"
 
 
 export default async function getCategories(): Promise<CategoryType[]> {
-    const result = await fetch(`${process.env.DEPLOYMENT_URL}api/categories/`, {next: {tags: ["categories"]}})
+    const result = await fetch(`https://marketplace-nextjs-roan.vercel.app/api/categories/`, {next: {tags: ["categories"]}})
     .then((res) => res.json())
     .then((data) => data['data'] as CategoryType[])
     return result
