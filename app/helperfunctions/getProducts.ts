4aae3d6640bb06cd38ffe85cc4ea5a78f5e0a5c6
Adam Kubik <kubik.adam445@gmail.com>
2024-01-18 13:31:29 +0100

    trying some env stuff

--- app/helperfunctions/getProducts.ts
+++ app/helperfunctions/getProducts.ts
@@ -2,7 +2,7 @@ import { ProductType } from "../schemas"
 
 
 export default async function getProducts(category: string): Promise<ProductType[]> {
-    const result = await fetch(`https://marketplace-nextjs-roan.vercel.app/api/products/category/${category}/`, {next: {tags: ["products"]}})
+    const result = await fetch(`${process.env.DEPLOYMENT_URL}api/products/category/${category}/`, {next: {tags: ["products"]}})
     .then(res => res.json())
     .then(data => data['data'] as ProductType[])
     return result
