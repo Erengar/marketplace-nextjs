ff84a58578ff0628556e65e8a7460b8f95530749
Adam Kubik <kubik.adam445@gmail.com>
2024-01-22 20:54:26 +0100

    added hover z index to category component, now category on front page shows correctly during animation

--- app/components/client/Category.tsx
+++ app/components/client/Category.tsx
@@ -11,7 +11,7 @@ export default function Category({category}: {category: CategoryType}) {
     //This is a hacky way to make sure the description is on top of the other categories
     const startHover = () => {
         setShowDescription(true)
-        setTimeout(() => {document.getElementById("category-description")?.classList.add('z-20')}, 400)
+        setTimeout(() => {document.getElementById("category-description")?.classList.add('z-20')}, 500)
     }
     const endHover = () => {
         document.getElementById("category-description")?.classList.remove('z-20')
@@ -26,14 +26,12 @@ export default function Category({category}: {category: CategoryType}) {
             className={`dark:bg-slate-950 dark:hover:bg-slate-900 dark:hover:scale-110 rotate-45
             dark:active:outline-1 dark:active:outline-slate-700 dark:active:scale-110
             text-sky-900 bg-slate-200 ${showDescription && "bg-slate-400"} active:outline-1 active:outline-neutral-400 active:scale-110
-            font-semibold border border-solid rounded-lg min-w-fit min-h-fit max-w-sm max-h-sm h-40 w-40 flex justify-center items-center z-10`}
+            font-semibold border border-solid rounded-lg min-w-fit min-h-fit max-w-sm max-h-sm h-40 w-40 flex justify-center items-center z-10 hover:z-30`}
             key={category.name}
-            initial={false}
             animate={showDescription? {rotate:[45,0]} : {rotate:[0, 45]}}
             transition={{duration: 0.3, ease:'easeInOut'}}
             >
                 <motion.span className="mx-5"
-                initial={{rotate:-45}}
                 animate={showDescription? {rotate:[-45,0]} : {rotate:[0,-45]}}
                 transition={{duration: 0.3, ease:'easeInOut'}}
                 >
