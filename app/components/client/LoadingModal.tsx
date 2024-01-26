78ea8f5062b7248ac26e2d2886e38bd595089d20
Adam Kubik <kubik.adam445@gmail.com>
2024-01-26 15:56:53 +0100

    changes to loading modal

--- app/components/client/LoadingModal.tsx
+++ app/components/client/LoadingModal.tsx
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 
 export default function LoadingModal({text, backDrop=true, seeThrough=false}: {text: string, backDrop?: boolean, seeThrough?: boolean}) {
     return (
-        <div className={`fixed top-0 right-0 left-0 flex justify-center items-center w-full h-full max-h-full ${backDrop && "bg-gray-500 bg-opacity-50 z-10"}`}>
+        <div className={`z-10 fixed top-0 right-0 left-0 flex justify-center items-center w-full h-full max-h-full ${backDrop && "bg-gray-500 bg-opacity-50"}`}>
             <motion.div
-            className={`min-w-[200px] max-w-6xl min-h-min z-20 rounded bg-white border border-black border-solid p-2 flex flex-col place-items-center gap-y-2 ${seeThrough && "bg-opacity-50 bg-black"}`}
+            className={`${seeThrough && !text ? null : "min-w-[200px]"} max-w-6xl min-h-min z-20 rounded border border-black border-solid p-2 flex flex-col place-items-center gap-y-2 ${seeThrough && "bg-opacity-50 bg-black"}`}
             initial={{scale:0}}
             animate={{scale:1}}
             exit={{scale:0}}
