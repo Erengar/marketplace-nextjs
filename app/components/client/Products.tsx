76e2319bb1dbdd71cfdc4d5f076e0a358fda5f54
Adam Kubik <kubik.adam445@gmail.com>
2024-01-27 12:08:20 +0100

    testing production build

--- app/components/client/Products.tsx
+++ app/components/client/Products.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import SkeletonProducts from "../server/SkeletonProducts";
 import Pagination from "./Pagination";
 import ProductSort from "./ProductSort";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Products({category}: {category:string}) {
     const [products, setProducts] = useState<ProductType[]>();
@@ -13,14 +13,29 @@ export default function Products({category}: {category:string}) {
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage, setItemsPerPage] = useState(40);
     const [sortSignal, setSortSignal] = useState("name");
+    const url = usePathname()
     const router = useRouter();
-
+    const query = useSearchParams()
+    
     // Set items per page depending on screen size, 20 for mobile, 50 for desktop
     useEffect(() => {
         window.innerWidth < 768 ? setItemsPerPage(20) : setItemsPerPage(50)
     },[])
+    
+    useEffect(() => {
+        if (query.get("sort")) {
+            setSortSignal(query.get("sort")!)
+        }
+        if (query.get("currentpage")) {
+            setCurrentPage(parseInt(query.get("currentpage")!))
+        }
+    }, [query])
+
 
     useEffect(() => {
+        //This is ensuring that user can use forward and backwards navigation in browser
+        router.push(url + `?sort=${sortSignal}&currentpage=${currentPage}`)
+
         fetch(`/api/products/?currentpage=${currentPage}&itemsperpage=${itemsPerPage}&category=${category}&sort=${sortSignal}`, {next: {tags: ["products"]}})
         .then((res) => res.json())
         .then((data) => {
