6855043d7bd53580b79af5f9609deda1b9d3822b
Adam Kubik <kubik.adam445@gmail.com>
2024-02-07 17:58:16 +0100

    final bugfix of LoadingHorizontal component

--- app/components/client/LoadingHorizontal.tsx
+++ app/components/client/LoadingHorizontal.tsx
@@ -3,21 +3,20 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 export default function LoadingHorizontal() {
-    const [width, setWidth] = useState<number | undefined>(undefined);
+    const [width, setWidth] = useState<number>(1980);
 
     useEffect(() => {
         if (typeof window !== "undefined") {
             setWidth(window.innerWidth);
         }
     }, []);
-    const duration = width! > 768 ? 1.5 : 1.2;
     return (
         <div className={`flex h-0.5 w-full bg-slate-100`}>
             <motion.div
                 className="h-full w-8 bg-blue-500 md:w-16"
                 animate={{ x: [-100, width] }}
                 transition={{
-                    duration: duration,
+                    duration: 1.2,
                     repeat: Infinity,
                     ease: "easeIn",
                 }}
